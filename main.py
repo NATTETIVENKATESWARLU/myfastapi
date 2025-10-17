d5726b9d97f382203d3b2c9d4530db5618988ce3
NATTETIVENKATESWARLU <n.venkateswarlu180@gmail.com>
2025-10-17 21:59:30 +0530

    this post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Request
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,3 +12,17 @@ async def read_root():
 @app.get("/")
 async def read_root1():
     return {"message": "This is the second root endpoint."}
+
+#add post request endpoint
+@app.post("/items/")
+async def create_item(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Item created successfully!", "item": payload}
+
+
+#add another post request endpoint ui asking payload from request
+@app.post("/items1/")
+async def create_item1(request: Request):
+    payload = await request.json()
+    print(payload)
+    return {"message": "Item created successfully!", "item": payload}
